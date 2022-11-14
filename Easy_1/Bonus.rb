7d5528b4c0b31cd17a75412e959e56650fa3bba2
johnsonmayae <johnson.mayae@gmail.com>
2022-11-14 11:22:48 +0900

    Lesson 1 Questions

---
+++ Easy_1/Bonus.rb
@@ -0,0 +1,16 @@
+def calculate_bonus(num, boolean)
+  if boolean == true 
+    num / 2
+  else 
+    0
+  end
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
+
+# Solution 
+# def calculate_bonus(salary, bonus)
+# bonus ? (salary / 2) : 0
+# end
