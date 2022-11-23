a06071c520f7071c7260fb23b09dcaee1c27c184
johnsonmayae <johnson.mayae@gmail.com>
2022-11-23 12:10:02 +0900

    lesson_3 commit again

---
+++ lesson_3/practice_problems_hard_1.rb
@@ -0,0 +1,23 @@
+# Question 1
+# undefined method error
+
+# Question 2
+# prints "hi there"
+
+# Question 3
+# A) prints "one is: one, two is: two, three is: three"
+# B) prints "one is: one, two is: two, three is: three"
+# C) prints "one is: two, two is: three, three is: one"
+
+# Question 4
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  true
+  
+end
